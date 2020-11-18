5b2447b1feb6347ba20117f525a72ff50c36b801
freelancerifaz <mrifaz369@gmail.com>
2020-11-19 01:15:14 +0600

    initial added all file

---
+++ src/components/HeaderMain/HeaderMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderMain = () => {
+    return (
+        <div>
+            <h1>This is headermain</h1>
+        </div>
+    );
+};
+
+export default HeaderMain;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import HeaderMain from '../HeaderMain/HeaderMain';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <HeaderMain></HeaderMain>
+        </div>
+    );
+};
+
+export default Home;
